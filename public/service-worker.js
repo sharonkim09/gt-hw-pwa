28eca4316ae0f05cf2d90b1fa4cddafe23f7a370
Sharon Kim <sharonkim1709@gmail.com>
2020-07-28 11:57:49 -0400

    added db caching for offline transactions to indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
     "service-worker.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "/styles.css"
+    "/styles.css",
+    "/db.js"
   ];
 
 const CACHE_NAME = "static-cache-v1";
